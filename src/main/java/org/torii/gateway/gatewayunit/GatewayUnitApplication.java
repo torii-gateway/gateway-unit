fbe36067ad27275d2fd0f3a85ca58a05d8e0d979
g-terra <by.terra@hotmail.com>
2023-06-05 23:06:06 +0200

    initial commit

---
+++ src/main/java/org/torii/gateway/gatewayunit/GatewayUnitApplication.java
@@ -0,0 +1,13 @@
+package org.torii.gateway.gatewayunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayUnitApplication.class, args);
+    }
+
+}
