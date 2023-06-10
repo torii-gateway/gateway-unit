c77fd8ac003b1e7fd7f2b066e6a2683945d4b532
g-terra <by.terra@hotmail.com>
2023-06-10 09:51:28 +0200

    implementation for load balancing

--- src/main/java/org/torii/gateway/gatewayunit/config/UpstreamServicesConfiguration.java
+++ src/main/java/org/torii/gateway/gatewayunit/config/UpstreamServicesConfiguration.java
@@ -2,18 +2,20 @@ package org.torii.gateway.gatewayunit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.torii.gateway.gatewayunit.domain.Server;
 import org.torii.gateway.gatewayunit.domain.UpstreamService;
 
 import java.util.List;
 
+
 @Configuration
 public class UpstreamServicesConfiguration {
-
 
     @Bean
     public List<UpstreamService> upstreamServices() {
         return List.of(
-                new UpstreamService("service-1", "http://localhost:8081")
+                new UpstreamService("mock-service-1", List.of(Server.fromString("http://localhost:8081"), Server.fromString("http://localhost:8082"))),
+                new UpstreamService("mock-service-2", List.of(Server.fromString("http://localhost:8083"), Server.fromString("http://localhost:8084")))
         );
     }
 
--- src/main/java/org/torii/gateway/gatewayunit/rest/UnitController.java
+++ src/main/java/org/torii/gateway/gatewayunit/rest/UnitController.java
@@ -2,16 +2,23 @@ package org.torii.gateway.gatewayunit.rest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.*;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer.Factory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
-import org.torii.gateway.gatewayunit.domain.UpstreamService;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -21,7 +28,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 public class UnitController {
     private final WebClient webClient;
 
-    private final List<UpstreamService> upstreamUpstreamServices;
+    private final Factory<ServiceInstance> loadBalancerFactory;
 
     @RequestMapping(path = "/{serviceRef}/**", method = {GET, POST, PUT, DELETE})
     public Mono<ResponseEntity<String>> handleAllMethods(
@@ -30,47 +37,42 @@ public class UnitController {
             @PathVariable String serviceRef
     ) {
 
-        if (upstreamUpstreamServices.stream().noneMatch(s -> s.id().equals(serviceRef))) {
-            return Mono.error(
-                    new ResponseStatusException(
-                            HttpStatus.NOT_FOUND,
-                            "Upstream service not found")
-            );
-        }
+        ReactiveLoadBalancer<ServiceInstance> loadBalancer = loadBalancerFactory.getInstance(serviceRef);
+
 
         String path = request.getPath()
                 .pathWithinApplication()
                 .value()
                 .substring(serviceRef.length() + 1);
 
-        String uri = upstreamUpstreamServices.stream()
-                .filter(s -> s.id().equals(serviceRef))
-                .findFirst()
-                .map(UpstreamService::host)
-                .map(url -> url + path)
-                .orElseThrow();
 
         HttpMethod method = request.getMethod();
 
-        WebClient.RequestBodySpec requestSpec = webClient.method(method).uri(uri);
 
-        if (Objects.equals(method, HttpMethod.POST) || Objects.equals(method, HttpMethod.PUT)) {
-            requestSpec.contentType(Objects.requireNonNull(request.getHeaders().getContentType()));
-        }
+        Flux<Mono<ResponseEntity<String>>> monoFlux = Flux.from(loadBalancer.choose()).map(i -> {
 
-        Mono<Mono<ResponseEntity<String>>> monoMono = requestBody.map(body -> requestSpec
-                .bodyValue(body)
-                .exchangeToMono(response -> response.toEntity(String.class))
-        ).defaultIfEmpty(
-                requestSpec
-                        .exchangeToMono(response -> response.toEntity(String.class))
-        );
+            String uri = i.getServer().getUri() + path;
 
-        return monoMono.flatMap(mono -> mono)
-                .onErrorResume(ResponseStatusException.class, ex -> {
-                    log.error("Error occurred while handling request", ex);
-                    return Mono.just(ResponseEntity.status(ex.getStatusCode()).body(ex.getReason()));
-                });
+            WebClient.RequestBodySpec requestSpec = webClient.method(method).uri(uri);
+
+            if (Objects.equals(method, HttpMethod.POST) || Objects.equals(method, HttpMethod.PUT)) {
+                requestSpec.contentType(Objects.requireNonNull(request.getHeaders().getContentType()));
+            }
+
+            return requestBody.map(body -> requestSpec
+                    .bodyValue(body)
+                    .exchangeToMono(response -> response.toEntity(String.class))
+            ).defaultIfEmpty(
+                    requestSpec.exchangeToMono(response -> response.toEntity(String.class))
+            );
+        }).flatMap(Function.identity());
+
+
+        return monoFlux.next().flatMap(Function.identity()).onErrorResume(ResponseStatusException.class, ex -> {
+            log.error("Error occurred while handling request", ex);
+            return Mono.just(ResponseEntity.status(ex.getStatusCode()).body(ex.getReason()));
+        });
+
     }
 
 }
